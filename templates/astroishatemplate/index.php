36afe487257475e497b783e8af29637c9b542210
luffy22 <kopnite@gmail.com>
2018-12-04 14:37:13 +0530

    bootstrap added to template added 2018-12-04 @work

--- templates/astroishatemplate/index.php
+++ templates/astroishatemplate/index.php
@@ -3,15 +3,25 @@
 <html xmlns="http://www.w3.org/1999/xhtml" 
    xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
-<jdoc:include type="head" />
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="Comprehensive Website For Vedic Astrology" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/system.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/general.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
-<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery.min.css" language="javascript"></script>
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/bootstrap.min.css" type="text/css" />
+<link href="/aisha/index.php?format=feed&amp;type=rss" rel="alternate" type="application/rss+xml" title="RSS 2.0" />
+<link href="/aisha/index.php?format=feed&amp;type=atom" rel="alternate" type="application/atom+xml" title="Atom 1.0" />
+<link href="/aisha/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery.min.js" language="javascript"></script>
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery-migrate.min.js" language="javascript"></script>
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/bootstrap.min.js" language="javascript"></script>
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/popper.min.js" language="javascript"></script>
 </head>
 <body>
-<jdoc:include type="modules" name="top" /> 
+<div class="container-fluid">
+<jdoc:include type="modules" name="content1" /> 
 <jdoc:include type="component" />
-<jdoc:include type="modules" name="bottom" />
+<jdoc:include type="modules" name="footer" />
+</div>
 </body>
 </html>
