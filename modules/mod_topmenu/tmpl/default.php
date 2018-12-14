d321b0e47f0ac4daa4487afd9e4b2ecfe8ff02bc
luffy22 <kopnite@gmail.com>
2018-12-14 08:25:27 +0530

    top menu finished 2018-12-13 @home

--- modules/mod_topmenu/tmpl/default.php
+++ modules/mod_topmenu/tmpl/default.php
@@ -7,8 +7,8 @@ defined('_JEXEC') or die;
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="top-menu">
-        <a class="navbar-brand"><img src="images/logo.png" alt="logo" /></a>
-        <span class="navbar-text">Astro Isha</span>
+        <!--<a class="navbar-brand"><img src="images/logo.png" alt="logo" /></a>
+        <span class="navbar-text">Astro Isha</span>-->
         <ul class="navbar-nav">
         <?php
             foreach($topmenu as $menu)
@@ -18,22 +18,35 @@ defined('_JEXEC') or die;
                 <?php
                 if($menu->level == "2")
                 {
-                    $menu_id    = $menu->id;
-                    
+                    $p_menu = $app->getMenu();
+                    $children       = $p_menu->getItems('parent_id',$menu->id, false);
+                    $count          = count($children);
+                    $i=0;  // counter to increment for divider
                 ?>
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="top-1" href="#"><?php echo $menu->title; ?></a>
                     <div class="dropdown-menu navbar-dark bg-dark">
-                 <?php
-          
-                        if($menu->level == "3" && $menu->parent_id == $menu_id)
+                <?php
+                    foreach($children as $child)
+                    {
+                        if($child->type=="url")
                         {
-                            echo "calls";exit;
-                 ?>
-                        <a class="dropdown-item" href="#"><?php echo $menu->title; ?></a>
-                        <div class="dropdown-divider"></div>                   
-                        <?php   
+                            $url    = JRoute::_($child->link);
                         }
- 
+                        else
+                        {
+                            $url   = JRoute::_($child->link . "&Itemid=" . $child->id);
+                        }
+                ?>
+                        <a class="dropdown-item" href="<?php echo $url ?>"><?php echo $child->title; ?></a>
+                <?php
+                        if($i+1 < $count)
+                        {
+                ?>
+                        <div class="dropdown-divider"></div>
+                <?php
+                        }
+                        $i++;
+                    }
                 ?>
                     </div>
                 <?php
@@ -47,3 +60,5 @@ defined('_JEXEC') or die;
         </ul>
     </div>
 </nav>
+<!--<a class="dropdown-item" href="#"><?php //echo $menu->title; ?></a>
+                        <div class="dropdown-divider"></div>-->              
