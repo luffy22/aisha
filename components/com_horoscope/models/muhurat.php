5577035de21d64e2582e273dbf55bb579ea89ea6
luffy22 <kopnite@gmail.com>
2019-02-27 15:03:39 +0530

    com_horoscope/muhurat sunrise, sunset done 2019-02-27 @work

--- components/com_horoscope/models/muhurat.php
+++ components/com_horoscope/models/muhurat.php
@@ -11,14 +11,26 @@ class HoroscopeModelMuhurat extends HoroscopeModelLagna
     public function getData()
     {
         $libPath        = JPATH_BASE.'/sweph/';
-        $jinput         = JFactory::getApplication()->input;
+        $jinput         = JFactory::getApplication()->input;        
+        $date           = date('Y-m-d');
+        $location       = $jinput->get('location', 'default_value', 'filter');
+        $location       = explode("_",$location);
+        $timezone       = "Asia/Kolkata";
+        $sun_times      = $this->getSunTimings($date, $timezone);
+        print_r($sun_times);exit;
+        $moon_times     = $this->getMoonTimings($date,$timezone);
+        //print_r($moon_times);exit;
         
-        $dtz = new DateTimeZone("Asia/Kolkata"); //Your timezone
-        $date = new DateTime(date("Y-m-d H:i:s"), $dtz);
+    }
+    public function getSunTimings($date, $timezone)
+    {
+        $dtz            = new DateTimeZone($timezone); //Your timezone
+        $date           = new DateTime($date, $dtz);
+        $date->sub(new DateInterval('P1D'));
         //echo $date->format('d.m.Y H:i:s');exit;        
        
         $date           = $date->format('d.m.Y');
-        $long           = "72.57";
+        $lon            = "72.57";
         $lat            = "23.02";
         //$date = date('d.m.Y', $utcTimestamp);
         //$time = date('H:i:s', $utcTimestamp);
@@ -27,35 +39,55 @@ class HoroscopeModelMuhurat extends HoroscopeModelLagna
         $output = "";
         //echo $utcTimestamp;exit;
         //echo date('Y-m-d H:i:s', $utcTimestamp); echo '<br>';
-        exec ("swetest -b$date -topo[$long,$lat,0] -rise  -hindu -p0 -head", $output);
-        print_r($output);exit;
-
-        # OUTPUT ARRAY
-        # Planet Name, Planet Degree, Planet Speed per day
-        //$asc            = $this->getAscendant($result);
-        $planets        = $this->getPlanets($output);
-        $data           = array();
-        foreach($planets as $planet=>$dist)
+        exec ("swetest -b$date -geopos$lon,$lat,0 -rise -hindu -p0 -n12 -head", $output);
+        $timings        = $output;$i = 0;
+        $sun            = array();
+        foreach($timings as $result)
         {
-            $dist2          = $this->convertDecimalToDegree($dist, "details");
-            $sign           = $this->calcDetails($dist);
-            $navamsha       = $this->getNavamsha($planet,$sign,$dist2);
-            $dist           = array($planet."_dist"=>$dist2);
-            $details        = array($planet."_sign"=>$sign);
-            $data           = array_merge($data, $dist, $details, $navamsha);
+            if($i == 0){$i++;continue;}
+            else{
+               $split       = explode("rise",$result);
+               $split       = explode("set",$split[1]);
+               $times       = array("sun_rise_".$i=>$split[0],"sun_set_".$i=>$split[1]);
+               $sun         = array_merge($sun, $times);$i++;
+            }
+            
         }
-        //print_r($data);exit;
-        $moon_sign          = $data['Moon_sign'];                  
-        $moon_dist          = $data['Moon_dist'];
-        $moon_nakshatra     = $this->getNakshatra($moon_sign, $moon_dist);
-        $nakshatra_deg      = $this->getNakshatraDeg($moon_sign, $moon_nakshatra, $moon_dist);
-        $get_dasha          = $this->getDashaPeriod($dob_tob, $nakshatra_deg);
-        $period_id          = $get_dasha['dob_period_id'];$dasha_end    = $get_dasha['dob_sub_end'];
-        //$get_current        = $this->getCurrentPeriod($period_id, $dasha_end);
-        $get_remain_dasha   = array("get_remain_dasha"=>$this->getRemainDasha($period_id, $dasha_end));
-        $data               = array_merge($data, $get_dasha,$get_remain_dasha);
-        return $data;
+        return $sun;
+    }
+    public function getMoonTimings($date, $timezone)
+    {
+        $dtz            = new DateTimeZone($timezone); //Your timezone
+        $date           = new DateTime($date, $dtz);
+        $date->sub(new DateInterval('P1D'));
+        //echo $date->format('d.m.Y H:i:s');exit;        
+       
+        $date           = $date->format('d.m.Y');
+        $lon            = "72.57";
+        $lat            = "23.02";
+        //$date = date('d.m.Y', $utcTimestamp);
+        //$time = date('H:i:s', $utcTimestamp);
         
+        $h_sys = 'P';
+        $output = "";
+        //echo $utcTimestamp;exit;
+        //echo date('Y-m-d H:i:s', $utcTimestamp); echo '<br>';
+        exec ("swetest -b$date -geopos$lon,$lat,0 -rise -hindu -p1 -n12 -head", $output);
+        //print_r($output);exit;
+        $timings        = $output;$i = 0;
+        $moon           = array();
+        foreach($timings as $result)
+        {
+            if($i == 0){$i++;continue;}
+            else{
+               $split       = explode("rise",$result);
+               $split       = explode("set",$split[1]);
+               $times       = array("moon_rise_".$i=>$split[0],"moon_set_".$i=>$split[1]);
+               $moon        = array_merge($moon, $times);$i++;
+            }
+            
+        }
+        return $moon;
     }
 
 }
