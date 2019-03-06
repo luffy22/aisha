e2205f51bfddc48cbbf393ae183bae4e544e72f9
luffy22 <kopnite@gmail.com>
2019-03-06 14:44:02 +0530

    com_horoscope/muhurat abhijit done 2019-03-06 @work

--- components/com_horoscope/models/muhurat.php
+++ components/com_horoscope/models/muhurat.php
@@ -91,9 +91,15 @@ class HoroscopeModelMuhurat extends HoroscopeModelLagna
         $array              = array($planet."_rise_".$num=>$date->format('d-m-Y H:i:s'),$planet."_set_".$num=>$date1->format('d-m-Y H:i:s'));
         return $array;
     }
+    /*
+     * Get the Muhurat times for the particular day
+     * @param datetime Date and Time for sunrise and sunset
+     * @return array Array with Muhurat times for particular day.
+     */
     public function getMuhurat($times)
     {
-        
+        $db                 = JFactory::getDbo();  // Get db connection
+        $query              = $db->getQuery(true);
         //print_r($times);exit;
         for($i=1;$i<13;$i++)
         {
@@ -103,17 +109,71 @@ class HoroscopeModelMuhurat extends HoroscopeModelLagna
             }
             else
             {
-                $j          = $i+1;
-                $rise       = new DateTime($times['sun_rise_'.$i]);//echo $rise."<br/>";
-                $set        = new DateTime($times['sun_set_'.$i]);//echo $set."<br/>";
-                $rise_next  = new DateTime($times['sun_rise_'.$j]);//echo $rise_next."<br/><br/>";exit;
-                $day_interval = $set->getTimestamp() - $rise->getTimestamp();//echo $day_interval;exit;
-                $day_prahar   = $day_interval/8;//echo $day_prahar;exit;
-                $night_interval = $rise_next->getTimestamp() - $set->getTimeStamp();
-                $night_prahar   = $night_interval/8;//echo $night_prahar;exit;
-                
+                $j                  = $i+1;
+                $rise               = new DateTime($times['sun_rise_'.$i]);//echo $rise."<br/>";
+                $day_of_week        = $rise->format('l');
+                $set                = new DateTime($times['sun_set_'.$i]);//echo $set."<br/>";
+                $rise_next          = new DateTime($times['sun_rise_'.$j]);//echo $rise_next."<br/><br/>";exit;
+                $day_interval       = $set->getTimestamp() - $rise->getTimestamp();//echo $day_interval;exit;
+                $night_interval     = $rise_next->getTimestamp() - $set->getTimeStamp();
+                $day_prahar         = $this->getPrahar($times['sun_rise_'.$i],$day_interval,"day");
+                //print_r($day_prahar);exit;
+                $night_prahar       = $this->getPrahar($times['sun_set_'.$i],$night_interval,"night");
+                $query              ->select($db->quoteName('prahar_num'));
+                $query              ->from($db->quoteName('#__horo_muhurat'));
+                $query              ->where($db->quoteName('day').'='.$db->quote($day_of_week));
+                $db                 ->setQuery($query);
+                $result             = $db->loadAssocList();
+                $rahu               = $result[0]['prahar_num'];//echo $rahu_kalam."<br/>";
+                $yama               = $result[1]['prahar_num'];//echo $yama_ghanta."<br/>";
+                $guli               = $result[2]['prahar_num'];//echo $guli_kalam."<br/>";exit;
+                $rahu_kalam         = array("rahu_kalam_start"=>$day_prahar["day_prahar_start_".$rahu],
+                                            "rahu_kalam_end"=>$day_prahar["day_prahar_end_".$rahu]);
+                $yama_kalam         = array("yama_kalam_start"=>$day_prahar["day_prahar_start_".$yama],
+                                            "yama_kalam_end"=>$day_prahar["day_prahar_end_".$yama]);
+                $guli_kalam         = array("guli_kalam_start"=>$day_prahar["day_prahar_start_".$guli],
+                                            "guli_kalam_end"=>$day_prahar["day_prahar_end_".$guli]);
+                $abhijit_kalam      = $this->getAbhijitKalam($times['sun_rise_'.$i],$day_interval);
+                //print_r($night_prahar);exit;
                 //echo $night_interval;exit;
             }
         }
+    }
+    public function getPrahar($date, $interval,$day_night)
+    {
+        $prahar                     = array();      // total number for prahar
+        $date                       = new DateTime($date);
+        $prahar_sec                 = explode(".",round(($interval/8),2)); // total seconds divided by 8 as there are 8 prahars in a day
+        for($i=1;$i<9;$i++)
+        {
+            $start                  = $date->format('Y-m-d H:i:s');
+            $date                   ->add(new DateInterval('PT'.$prahar_sec[0].'S'));
+            $end                    = $date->format('Y-m-d H:i:s');
+            $prahar_period          = array($day_night."_prahar_start_".$i => $start,$day_night."_prahar_end_".$i =>$end);
+            $prahar                 = array_merge($prahar, $prahar_period);
+        }
+        return $prahar;
+    }
+    public function getAbhijitKalam($date, $interval)
+    {
+        $muhurat                    = explode(".",round(($interval/15),2));
+        $date                       = new DateTime($date);
+        $abhijit                    = array();
+        for($i=1;$i<16;$i++)
+        {
+            if($i == 8)
+            {
+                $start              = $date->format('Y-m-d H:i:s');
+                $date               ->add(new DateInterval('PT'.$muhurat[0].'S'));
+                $end                    = $date->format('Y-m-d H:i:s');
+                $period            = array("abhijit_start"=>$start,"abhijit_end"=>$end);
+                $abhijit            = array_merge($abhijit, $period);
+            }
+            else
+            {
+                $date               ->add(new DateInterval('PT'.$muhurat[0].'S'));
+            }
+        }
+        return $abhijit;
     }
 }
