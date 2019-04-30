891272ad25c237205e0f6030cf65c90d925ee38a
luffy22 <kopnite@gmail.com>
2019-04-30 15:02:02 +0530

    com_horoscope/panchang tithi in-progress 2019-04-30 @work

--- components/com_horoscope/models/panchang.php
+++ components/com_horoscope/models/panchang.php
@@ -16,13 +16,13 @@ class HoroscopeModelPanchang extends HoroscopeModelCalendar
         $date       = new DateTime($today, new DateTimeZone($tmz));
         $sunrise    = $this->getSunTimings($date->format('Y-m-d'), $tmz,$lat,$lon,$alt, 2);
         $moonrise   = $this->getMoonTimings($date->format('Y-m-d'), $tmz, $lat, $lon, $alt, 2);
-        $this->getPanchang();
+        $this->getPanchang($sunrise);
         //return array("date"=>$date->format('dS F Y'),"day_today"=>$date->format('l'),
                       //$sunrise, $moonrise);
     }
-    public function getPanchang()
+    public function getPanchang($sunrise)
     {
-        $date           = new DateTime("now");
+        $date           = new DateTime(date('29-04-2019'));
         $today          = $date->format('d.m.Y');
         $libPath        = JPATH_BASE.'/sweph/';
         putenv("PATH=$libPath");
@@ -30,12 +30,16 @@ class HoroscopeModelPanchang extends HoroscopeModelCalendar
         $output = "";
         $tithi_array    = array();
         // More about command line options: https://www.astro.com/cgi/swetest.cgi?arg=-h&p=0
+        //swetest -p6 -DD -b1.12.1900 -n100 -s5 -fPTZ -head
         exec ("swetest -edir$libPath -p1 -d0 -b$today -n2 -fPTl -head", $output);
-        $tithi          = $this->getTithiToday($date, $output);
+        //print_r($output);exit;
+        $tithi          = $this->getTithiToday($date, $output, $sunrise);
     }
     
-    public function getTithiToday($date, $output)
+    public function getTithiToday($date, $output, $sunrise)
     {
+        $rise1      = new DateTime($sunrise['sun_rise_1']); 
+        $rise1_dec  = $this->convertTimeToDecimal($rise1->format('H:i:s'));
         $array      = array();
         $i          = 1;
         foreach($output as $result)
@@ -47,7 +51,7 @@ class HoroscopeModelPanchang extends HoroscopeModelCalendar
             $date        ->add(new DateInterval('P1D'));
             if($first == "-")
             {
-                $tithi  = 180 + $tithi;
+                $tithi  = 360 + $tithi;
                 $tithi_doc  = array("paksha_".$i => "krishna", "tithi_".$i => $tithi);
                 $array      = array_merge($array, $tithi_doc);
             }
@@ -59,6 +63,46 @@ class HoroscopeModelPanchang extends HoroscopeModelCalendar
             }
             $i++;
         }
-       
+        //print_r($array);exit;
+        $tithi_lower        = number_format((float)$array['tithi_1'],2);
+        $tithi_upper        = number_format((float)$array['tithi_2'],2);
+        $tithi_abs          = round($tithi_lower, 0);
+        while($tithi_abs % 15 !== 0)
+        {
+            $tithi_abs++;
+            $tithi_abs_diff     = $tithi_abs - $tithi_lower;      // this is difference between lower tithi & change of tithi(90,105,120 etc)
+        } 
+        //echo $tithi_abs_diff;exit;
+        if($tithi_upper <  $tithi_lower)
+        {
+            $tithi_upper    = $tithi_upper + 360;
+        }
+        $tithi_diff         = $tithi_upper - $tithi_lower;
+        $tithi_diff         = number_format((float)$tithi_diff,2);
+        //echo $tithi_abs_diff." ".$rise1_dec." ".$tithi_diff;exit;
+        $tithi_change       = ($tithi_abs_diff*24)/$tithi_diff;
+        $tithi_change       = $this->convertDecimalToTime($tithi_change);
+        echo $tithi_change;exit;
+        
     }
+    public function convertDecimalToTime($dec)
+    {
+         // Converts decimal format to DMS ( Degrees / minutes / seconds ) 
+        $vars = explode(".",$dec);
+        $deg = $vars[0]%24;
+        $tempma = "0.".$vars[1];
+
+        $tempma = $tempma * 3600;
+        $min = floor($tempma / 60);
+        $sec = round($tempma - ($min*60),0);
+        return $deg.":".$min.":".$sec;
+    }
+    public function convertTimeToDecimal($time)
+    {
+        $hms = explode(":", $time);
+        $time   = $hms[0] + ($hms[1]/60) + ($hms[2]/3600);
+        $time   = round($time,2);
+        return $time;
+    }
+
 }
