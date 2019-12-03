a1770f1d77b4f5ea8fe0c344646870b51839f7ff
luffy22 <kopnite@gmail.com>
2019-12-03 18:27:21 +0530

    mod_order in-progress 2019-12-03 @work

--- modules/mod_order/helper.php
+++ modules/mod_order/helper.php
@@ -20,8 +20,8 @@ class ModOrderHelper
 {
     public function showOrder()
     {
-        $ip               = '117.196.1.11';
-        //$ip                 = '157.55.39.123';  // ip address
+        //$ip               = '117.196.1.11';
+        $ip                 = '157.55.39.123';  // ip address
         //$ip                 = '180.215.160.173';
         //$ip               = $_SERVER['REMOTE_ADDR'];        // uncomment this ip on server
 
@@ -43,7 +43,7 @@ class ModOrderHelper
         $service7           = 'sade-sati';
         if($info == "US")
         {
-            $query1          ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1          ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -52,7 +52,7 @@ class ModOrderHelper
         }
         else if($info == 'IN'||$info=='NP')
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -60,7 +60,7 @@ class ModOrderHelper
         }
         else if($info=='UK')
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -68,7 +68,7 @@ class ModOrderHelper
         }
         else if($info=='NZ')
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -76,7 +76,7 @@ class ModOrderHelper
         }
         else if($info=='CA')
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -84,7 +84,7 @@ class ModOrderHelper
         }
         else if($info=='SG')
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -92,7 +92,7 @@ class ModOrderHelper
         }
         else if($info=='AU')
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -101,7 +101,7 @@ class ModOrderHelper
         else if($info=='FR'||$info=='DE'||$info=='IE'||$info=='NL'||$info=='CR'||$info=='BE'
                 ||$info=='GR'||$info=='IT'||$info=='PT'||$info=='ES'||$info=='MT'||$info=='LV'||$info=='TR')
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -109,7 +109,7 @@ class ModOrderHelper
         }
         else if($info =='RU')
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
@@ -117,7 +117,7 @@ class ModOrderHelper
         }
          else
         {
-            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge')))
+            $query1         ->select($db->quoteName(array('a.country','a.amount','b.currency','b.curr_code','b.curr_full','a.service_for_charge','a.avail_disc','a.disc_percent','a.on_home','a.text_before','a.img_for_text')))
                             ->from($db->quoteName('#__expert_charges','a'))
                             ->join('INNER', $db->quoteName('#__user_currency', 'b') . ' ON (' . $db->quoteName('a.currency_ref') . ' = ' . $db->quoteName('b.Curr_ID') . ')')
                             ->where($db->quoteName('user_id').' = '.$db->quote($u_id).' AND '.
--- modules/mod_order/tmpl/default.php
+++ modules/mod_order/tmpl/default.php
@@ -5,23 +5,49 @@ defined('_JEXEC') or die;
 <?php
     if(JUri::current()  == JUri::base())
     {  
+        for($i=0;$i< count($order);$i++)
+        {
+            if($order[$i]['on_home'] == "yes" && ($order[$i]['service_for_charge'] == "long"||
+                    $order[$i]['service_for_charge'] == "short"))
+            {
+                $type       = substr($order[$i]['service_for_charge'],-5);
+                $discount   = round(($order[$i]['amount']*$order[$i]['disc_percent'])/100,2);
+                $disc_price = $order[$i]['amount'] - $discount;
+                $currency   = $order[$i]['currency'];
+                if($currency == "INR"){$pay_mode = "paytm";}else{$pay_mode = "paypal";}
 ?>
-<!--<div class="p-3 text-center alert alert-dark" id="touchslider">
-    <ul>
-        <li>
-    <a href="<?php echo JUri::base(); ?>">
-            <h3><img src="<?php //echo JUri::base().'images/ask-astroisha.png' ?>" height="80px" width="80px" /> Short Answer for <?php //echo " ".$order[1]['amount']." ".$order[0]['curr_code']." "; ?>only
-
-            </h2></a></li>
-        <li>
-    <a href="<?php echo JUri::base(); ?>">
-            <h3><img src="<?php //echo JUri::base().'images/ask-astroisha.png' ?>" height="80px" width="80px" /> Detailed Answer for <?php //echo " ".$order[0]['amount']." ".$order[0]['curr_code']." "; ?>only
-
-            </h2></a></li>
-        <li></li>
-    </ul>
-</div>-->
+<div class="p-3 row alert alert-dark">
+    
+    <div class="col-4"><img src="<?php echo JUri::base().'images/'.$order[$i]['img_for_text'] ?>" align="right"  /></div>
+    <div class="col-8"><br/><a href="<?php echo JUri::base().'ask-question?uname=luffy22&ques=1&type='.$type.'&fees='.$disc_price.'_'.$currency.'&pay_mode='.$pay_mode; ?>"><p class="lead"> <?php echo $order[$i]['text_before']." <s>".$order[$i]['amount']."&nbsp;".$order[$i]['curr_code']."</s> "; ?>
+            <br/><?php echo $disc_price." ".$order[$i]['curr_code']; ?> only
+        </p></a></div>          
+</div>
 <?php
+            }
+            else if($order[$i]['on_home'] == "yes" && ($order[$i]['service_for_charge'] == "life"||
+                    $order[$i]['service_for_charge'] == "marriage" ||
+                    $order[$i]['service_for_charge'] == "career" ||
+                    $order[$i]['service_for_charge'] == "sade-sati"||
+                    $order[$i]['service_for_charge'] == "yearly"))
+            {
+                $type       = $order[$i]['service_for_charge'];
+                $discount   = round(($order[$i]['amount']*$order[$i]['disc_percent'])/100,2);
+                $disc_price = $order[$i]['amount'] - $discount;
+                $currency   = $order[$i]['currency'];
+                if($currency == "INR"){$pay_mode = "paytm";}else{$pay_mode = "paypal";}
+?>
+<div class="p-3 row alert alert-dark">
+    
+    <div class="col-4"><img src="<?php echo JUri::base().'images/'.$order[$i]['img_for_text'] ?>" align="right"  /></div>
+    <div class="col-8"><br/><a href="<?php echo JUri::base().'order-report?report='.$type.'&fees='.$disc_price.'_'.$currency.'&pay_mode='.$pay_mode ?>"><p class="lead"> <?php echo $order[$i]['text_before']." <s>".$order[$i]['amount']."&nbsp;".$order[$i]['curr_code']."</s> "; ?>
+            <br/><?php echo $disc_price." ".$order[$i]['curr_code']; ?> only
+        </p></a></div>          
+</div>
+<?php
+            }
+             
+        }
     }
 ?>
 <div class="mb-3"></div>
